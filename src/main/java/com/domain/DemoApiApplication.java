c0c2d67e4d59dd8f7c5221ad3a758897c9aec89b
Cory S <mochamadcory.if03@gmail.com>
2022-01-15 23:37:58 +0700

    added all of DTO Stuff and Controller

--- src/main/java/com/domain/DemoApiApplication.java
+++ src/main/java/com/domain/DemoApiApplication.java
@@ -2,12 +2,17 @@ package com.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class DemoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApiApplication.class, args);
+	}
+
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
